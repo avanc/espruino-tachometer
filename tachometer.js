aedb5e237da2073aef6bafe037d95858377a0074
Sven Klomp <mail@klomp.eu>
2016-08-18 19:12:09 +0200

    Deactivate interval if speed is zero

--- tachometer.js
+++ tachometer.js
@@ -5,11 +5,15 @@ var counter = 0;
 var last_counter=0;
 var last_time=0;
 var velocity=0;
+var interval=0;
 
 setDeepSleep(1);
 
 var w=setWatch(function(e) {
   counter++;
+  if (!interval) {
+    interval=setInterval(calcVelocity, 2000);
+  }
   },
   pin,
   {repeat: 'true', edge:'rising'}
@@ -19,10 +23,15 @@ function getDistance() {
   return counter * circumference;
 }
 
-var i=setInterval(function(){
+
+function calcVelocity(){
   var current_time=getTime();
   var current_counter=counter;
   velocity=(current_counter-last_counter)*circumference/(current_time-last_time);
   last_time=current_time;
   last_counter=current_counter;
-}, 2000);
+  if (velocity===0) {
+    clearInterval(interval);
+    interval=0;
+  }
+}
