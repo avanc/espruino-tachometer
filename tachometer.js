80ca2b9836c28f73fca080361612fefb11b9e00f
Sven Klomp <mail@klomp.eu>
2016-08-22 18:06:34 +0200

    Ignore events for 50 ms for debouncing
    Debouncing using the build in method didn't work due to the very short signal.

--- tachometer.js
+++ tachometer.js
@@ -1,19 +1,22 @@
 var circumference = 2.11;
-var pin = BTN;
+var pin = B3;
 
 var counter = 0;
 var last_time=0;
 var RPM=0;
 
 setDeepSleep(1);
+pinMode(pin, 'input_pullup');
+
 
 var w=setWatch(function(e) {
+  if (e.time<last_time+0.05) return;
   RPM = 60 / (e.time - last_time);
   last_time=e.time;
   counter++;
   },
   pin,
-  {repeat: 'true', edge:'rising'}
+  {repeat: 'true', edge:'falling'}
 );
 
 function getDistance() {
